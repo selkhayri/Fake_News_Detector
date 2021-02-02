135f42dda7452403a07ea3bea78f59b7656746d1
Sami Elkhayri <selkhayri@gmail.com>
2021-02-01 20:46:38 -0500

    Started working on Classifier

---
+++ Data-Mining-With-Python/Final Exam/scraper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# /home/sami/anaconda3/bin/conda init bash
+#/home/sami/anaconda3/bin/conda activate pirple2
+export PYTHONPATH=/home/sami/libs/python
+/home/sami/anaconda3/envs/pirple2/bin/python /home/sami/education/DataScience/Pirple/Data-Mining-With-Python/Final\ Exam/API\ Scraper/scraper.py
+#/home/sami/anaconda3/bin/conda deactivate
+
