239515ec67c4906fa27d72677e6aa1e46cb90717
Sami Elkhayri <selkhayri@gmail.com>
2021-02-15 12:38:51 -0500

    Cleaning up

--- scraper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# /home/sami/anaconda3/bin/conda init bash
-#/home/sami/anaconda3/bin/conda activate pirple2
-export PYTHONPATH=/home/sami/libs/python
-/home/sami/anaconda3/envs/pirple2/bin/python /home/sami/education/DataScience/Pirple/Data-Mining-With-Python/Final\ Exam/API\ Scraper/scraper.py
-#/home/sami/anaconda3/bin/conda deactivate
-
